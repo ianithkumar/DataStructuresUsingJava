47654996ae72367cd5b81146038200a704007a55
ianithkumar <anithani1206@gmail.com>
2024-08-08 19:47:06 +0530

    Updated Linked List Package

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -60,6 +60,19 @@ public class LinkedList {
     }
   }
 
+  public void reverse(){
+    Node prev = null;
+    Node current = head;
+    Node next = null;
+    while(current!=null){
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    head = prev;
+  }
+
   public void show() {
     Node node = head;
     while (node.next != null) {
--- src/LinkedList/Runner.java
+++ src/LinkedList/Runner.java
@@ -4,33 +4,16 @@ import java.util.List;
 
 public class Runner {
   public static void main(String a[]){
-//    LinkedList list = new LinkedList();
-//    list.insert(18);
-//    list.show();
-//    list.insert(45);
-//    list.show();
-//    list.insert(12);
-//    list.show();
-//    list.insertAtStart(55);
-//    list.show();
-//    list.insertAt(1,5);
-//    list.show();
-//    list.deleteAt(3);
-//    list.show();
-//    list.deleteAt(4);
-//    list.show();
+    LinkedList list = new LinkedList();
+    list.insert(25);
+    list.insert(67);
+    list.insert(34);
+    list.insert(87);
+    list.insertAtStart(56);
+    list.insertAt(3,90);
+    list.show();
 
-    LinkedList list1 = new LinkedList();
-    list1.insert(1);
-    list1.insert(2);
-    list1.insert(4);
-    list1.show();
-
-    LinkedList list2 = new LinkedList();
-    list2.insert(1);
-    list2.insert(3);
-    list2.insert(4);
-    list2.show();
-
+    list.reverse();
+    list.show();
   }
 }
